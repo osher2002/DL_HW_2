99762467d520378ea5b302a102fb33fb3fe7e69a
osher2002 <96265260+osher2002@users.noreply.github.com>
2021-12-27 20:33:13 +0200

    Update and rename DL_HW_1.py to DL_HW_2.py

--- DL_HW_2.py
+++ DL_HW_2.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import roc_auc_score , recall_score
 from sklearn.metrics import confusion_matrix, precision_score
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import ResNet50, DenseNet121
 
 image_generator = ImageDataGenerator(
-    rotation_range=15,
-    width_shift_range=0.08,
-    shear_range=0.07,
-    zoom_range=0.09,
+    rotation_range=20,
+    width_shift_range=0.1,
+    shear_range=0.1,
+    zoom_range=0.1,
     samplewise_center=True,
     samplewise_std_normalization=True
 )
@@ -28,7 +29,7 @@ type0 = 'PNEUMONIA';
 pneumonia_size = len(os.listdir(os.path.join(train_directory, type0)))
 normal_size = len(os.listdir(os.path.join(train_directory, type1)))
 
-N = 100;
+N = 50;
 
 train_data = image_generator.flow_from_directory(train_directory, 
 batch_size=8, shuffle=True, class_mode='binary', target_size=(N, N))
@@ -44,99 +45,44 @@ type1_weigth = normal_size / (normal_size + pneumonia_size);
 
 class_weight = {0: type0_weigth, 1: type1_weigth}
 
-#DNN FULLY CONNECTED
-model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape = (N,N,3)),
-tf.keras.layers.Dense(1000, activation=tf.nn.relu),
-tf.keras.layers.Dense(250, activation=tf.nn.relu),
-tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)]);
+##ResNet Based Model
 
-model.compile(optimizer = tf.optimizers.Adam(),
+base_model = ResNet50(input_shape=(N,N,3), include_top=False, weights='imagenet')
+
+model = tf.keras.models.Sequential([
+        base_model,
+        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation="relu"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Dense(512, activation="relu"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Dense(128,activation="relu"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Dense(1,activation="sigmoid")
+])
+
+for layer_ in base_model.layers:
+    layer_.trainable = False
+    
+index_of_train = [87,170];
+
+base_model.layers[index_of_train[1]].trainable = True
+
+
+model.compile(optimizer = tf.keras.optimizers.Adam(lr=0.0005),
 loss = 'binary_crossentropy',
 metrics=['accuracy'])
 
 ### NUMBER OF EPOCHES !
-epochnum = 30;
+epochnum = 50;
 epochs_ = range(1,epochnum+1)
 
 history = model.fit(train_data, epochs=epochnum, validation_data=valid_data, 
-class_weight=class_weight, steps_per_epoch=8, validation_steps=8)
-
-#LOSS CALC & PLOTTING
-loss_train = history.history['loss']
-loss_val = history.history['val_loss']
-
-plt.plot(epochs_, loss_train, 'g', label='Training loss')
-plt.plot(epochs_, loss_val, 'b', label='validation loss')
-plt.title('Training and Validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
-
-#ACCURACY PLOTTING
-acc_train = history.history['accuracy']
-acc_val = history.history['val_accuracy']
-
-plt.plot(epochs_, acc_train, 'g', label='Training accuracy')
-plt.plot(epochs_, acc_val, 'b', label='validation accuracy')
-plt.title('Training and Validation accuracy')
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.legend()
-plt.show()
-
-predict = model.predict(test_data);
-
-test_predict = predict > 0.5;
-
-test_conf_mat = confusion_matrix(test_data.classes, test_predict);
-
-test_precision = precision_score(test_data.classes, test_predict);
-test_recall = recall_score(test_data.classes, test_predict);
-roc_auc_score(test_data.classes,predict);
-
-#CNN network for Classifying
-
-model = tf.keras.models.Sequential([
-    
-#The first convolution layer
-tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(N, N, 3)),
-tf.keras.layers.BatchNormalization(),
-tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
-tf.keras.layers.BatchNormalization(),
-tf.keras.layers.MaxPooling2D(2, 2),
-
-# The second convolution layer
-tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-tf.keras.layers.BatchNormalization(),
-tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-tf.keras.layers.BatchNormalization(),
-tf.keras.layers.MaxPooling2D(2,2),
-
-# The third convolution layer
-tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-tf.keras.layers.BatchNormalization(),
-tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-tf.keras.layers.BatchNormalization(),
-tf.keras.layers.MaxPooling2D(2,2),
-
-#Fully Connected
-tf.keras.layers.Flatten(),
-tf.keras.layers.Dense(128, activation='relu'),
-tf.keras.layers.Dropout(0.2),
-tf.keras.layers.Dense(1, activation='sigmoid')]);
-
-#Choosing Learning rate and analyze effect on results:
-
-model.compile(loss='binary_crossentropy',
-optimizer=tf.optimizers.Adam(learning_rate=0.005), metrics='accuracy')
-
-#FITTING THE MODEL
-epochnum = 30;
-epochs_ = range(1,epochnum+1)
-
-history = model.fit(train_data, epochs=epochnum, validation_data=valid_data, 
-class_weight=class_weight, steps_per_epoch=100, validation_steps=25)
+class_weight=class_weight, steps_per_epoch=100, validation_steps=100)
 
 #LOSS CALC & PLOTTING
 loss_train = history.history['loss']
@@ -172,12 +118,68 @@ test_precision = precision_score(test_data.classes, test_predict);
 test_recall = recall_score(test_data.classes, test_predict);
 auc_score = roc_auc_score(test_data.classes,predict);
 
+### DenseNet Based Model
+
+base_model = DenseNet121(input_shape=(N,N,3), include_top=False, weights='imagenet')
+
+model = tf.keras.models.Sequential([
+        base_model,
+        tf.keras.layers.GlobalMaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation="relu"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Dense(512, activation="relu"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Dense(128,activation="relu"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Dense(1,activation="sigmoid")
+])
+
+for layer_ in base_model.layers:
+    layer_.trainable = False
+    
+index_of_train = [213,420];
+
+base_model.layers[index_of_train[0]].trainable = True
 
 
+model.compile(optimizer = tf.keras.optimizers.Adam(lr=0.0005),
+loss = 'binary_crossentropy',
+metrics=['accuracy'])
 
+### NUMBER OF EPOCHES !
+epochnum = 50;
+epochs_ = range(1,epochnum+1)
 
+history = model.fit(train_data, epochs=epochnum, validation_data=valid_data, 
+class_weight=class_weight, steps_per_epoch=100, validation_steps=100)
 
+#LOSS CALC & PLOTTING
+loss_train = history.history['loss']
+loss_val = history.history['val_loss']
 
+plt.plot(epochs_, loss_train, 'g', label='Training loss')
+plt.plot(epochs_, loss_val, 'b', label='validation loss')
+plt.title('Training and Validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+#ACCURACY PLOTTING
+acc_train = history.history['accuracy']
+acc_val = history.history['val_accuracy']
+
+plt.plot(epochs_, acc_train, 'g', label='Training accuracy')
+plt.plot(epochs_, acc_val, 'b', label='validation accuracy')
+plt.title('Training and Validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
 
 
 
